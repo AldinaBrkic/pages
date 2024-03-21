e20bfcc78aebaff5571c9496a590e982656b5afd
AldinaBrkic <aldinabbe02@gmail.com>
2024-03-21 15:01:59 -0700

    the event has been added

--- forms/php/config.php
+++ forms/php/config.php
@@ -1,5 +1,5 @@
 <?php 
  
- $con = mysqli_connect("localhost","root","","forms") or die("Couldn't connect");
+ $con =  mysqli_connect("localhost","root","","forms") or die("Couldn't connect");
 
 ?>
